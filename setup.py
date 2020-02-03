0a52cee5f2cb270ba51e1d291fa481f67c859432
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-03 17:58:09 -0600

    create setup & move files to subdir

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="esportswiki_editing",
+    version="0.0.1",
+    author="RheingoldRiver",
+    author_email="river.esports@gmail.com",
+    description="River's wiki tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RheingoldRiver/esportswiki_editing",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=['mwclient', 'mwparserfromhell']
+)
