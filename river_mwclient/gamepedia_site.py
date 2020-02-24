f3faed83bc930fc4903718838f425e3aed76463e
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-23 20:26:18 -0600

    dont subclass GPSite

--- river_mwclient/gamepedia_site.py
+++ river_mwclient/gamepedia_site.py
@@ -2,7 +2,7 @@ from .extended_site import ExtendedSite
 from .cargo_site import CargoSite
 
 
-class GamepediaSite(ExtendedSite):
+class GamepediaSite(object):
     """
     Functions for connecting to and editing Gamepedia wikis. As with ExtendedSite, the focus of support
     is Gamepedia esports wikis specifically, but no functions here require using an esports wiki to work.
@@ -10,10 +10,16 @@ class GamepediaSite(ExtendedSite):
     Cargo-specific functions should be in CargoSite
     """
     cargo_client = None
+    client = None
 
-    def __init__(self, wiki: str, stg=False, username: str = None, password: str = None):
-        suffix = 'io' if stg else 'com'
-        wiki = '%s.gamepedia.' % wiki + suffix
-        super().__init__(wiki, path='/', username=username, password=password)
+    def __init__(self, wiki: str=None, stg=False,
+                 client: ExtendedSite = None,
+                 username: str = None, password: str = None):
+        if client:
+            self.client = client
+        else:
+            suffix = 'io' if stg else 'com'
+            wiki = '%s.gamepedia.' % wiki + suffix
+            self.client = ExtendedSite(wiki, path='/', username=username, password=password)
 
-        self.cargo_client = CargoSite(client=self)
+        self.cargo_client = CargoSite(client=self.client)
