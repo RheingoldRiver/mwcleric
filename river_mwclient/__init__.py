5d32ff26086a303de9520ff2f377130dfaae7171
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-23 18:18:34 -0600

    docs in login, import EsportsSite by default too

--- river_mwclient/__init__.py
+++ river_mwclient/__init__.py
@@ -1,3 +1,4 @@
-from .login import *
+from .login import login
 from .gamepedia_site import GamepediaSite
 from .esports_site import EsportsSite
+from .extended_site import ExtendedSite
--- river_mwclient/login.py
+++ river_mwclient/login.py
@@ -3,6 +3,9 @@ from .esports_site import EsportsSite
 
 def login(user, wiki, **kwargs):
     """
+    This function exists for backwards compatibility, but it also creates a stable wrapper for initializing
+    and logging into site objects. Eventually this message will be used, but until then I'd recommend to
+    use this wrapper any time you need to instantiate an esports site.
     :param user: Either "me" or "bot"
     :param wiki: Domain of a Gamepedia wiki
     :param kwargs: Sent directly to site.login
