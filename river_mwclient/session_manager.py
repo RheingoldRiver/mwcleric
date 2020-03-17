563f0188563d57616da828240c0f714669dd9095
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-03-16 23:31:54 -0500

    don't return cargo_client

--- river_mwclient/session_manager.py
+++ river_mwclient/session_manager.py
@@ -9,7 +9,7 @@ class SessionManager(object):
 
     def get_client(self, url: str = None, path: str = None, credentials: AuthCredentials = None, **kwargs):
         if url in self.existing_wikis:
-            return self.existing_wikis[url]['client'], self.existing_wikis[url]['cargo_client']
+            return self.existing_wikis[url]['client']
         client = Site(url, path=path, **kwargs)
         if credentials:
             client.login(username=credentials.username, password=credentials.password)
