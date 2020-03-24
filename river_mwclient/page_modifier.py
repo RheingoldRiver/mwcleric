dd20ee9c6686a4ecfb9e895ea5b7efbf96617796
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-03-24 05:03:15 -0500

    actually, not separating wikitext/plaintext was a bad idea

--- river_mwclient/page_modifier.py
+++ river_mwclient/page_modifier.py
@@ -7,6 +7,8 @@ class PageModifierBase(object):
     current_page = None
     current_text = None
     current_wikitext = None
+    prioritize_plaintext = False
+    prioritize_wikitext = False
 
     def __init__(self, site: WikiClient, page_list=None, limit=-1, summary=None, startat_page=None):
         self.summary = summary if summary else 'Bot Edit'
@@ -16,8 +18,11 @@ class PageModifierBase(object):
         self.passed_startat = False if startat_page else True
         self.startat_page = startat_page
 
-    def update_page(self):
-        raise PageModifierNotImplemented()
+    def update_wikitext(self):
+        self.prioritize_plaintext = True
+
+    def update_plaintext(self):
+        self.prioritize_wikitext = True
 
     def run(self):
         lmt = 0
@@ -33,9 +38,10 @@ class PageModifierBase(object):
             self.current_text = page.text()
             self.current_page = page
             self.current_wikitext = parse(self.current_text)
-            self.update_page()
+            self.update_plaintext()
+            self.update_wikitext()
             newtext = str(self.current_wikitext)
-            if newtext != self.current_page.text():
+            if newtext != self.current_page.text() and not self.prioritize_plaintext:
                 print('Saving page %s...' % page.name)
                 page.save(newtext, summary=self.summary)
             elif self.current_text != self.current_page.text():
