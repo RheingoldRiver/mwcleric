9017f581e005d6fbd4e14a9d728020c4c4a8cf4f
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-04-11 20:26:20 -0500

    Add support for quiet mode for cron tasks

--- river_mwclient/page_modifier.py
+++ river_mwclient/page_modifier.py
@@ -10,7 +10,19 @@ class PageModifierBase(object):
     prioritize_plaintext = False
     prioritize_wikitext = False
 
-    def __init__(self, site: WikiClient, page_list=None, title_list=None, limit=-1, summary=None, startat_page=None):
+    def __init__(self, site: WikiClient, page_list=None, title_list=None, limit=-1, summary=None, startat_page=None,
+                 quiet=False):
+        """Create a PageModifier object, which can perform operations to edit the plaintext
+        or wikitext of a page
+
+        :param site: a WikiClient to perform the edits on
+        :param page_list: a list of Page objects to operate on, do not use with title_list
+        :param title_list: a list of title strings to operate on, do not use with page_list
+        :param limit: stop after scanning this many pages
+        :param summary: edit summary
+        :param startat_page: skip to this page
+        :param quiet: don't print any console output (set to True for cron processes)
+        """
         if title_list is not None:
             page_list = [site.client.pages[p] for p in title_list]
         self.summary = summary if summary else 'Bot Edit'
@@ -19,11 +31,20 @@ class PageModifierBase(object):
         self.limit = limit
         self.passed_startat = False if startat_page else True
         self.startat_page = startat_page
+        self.quiet = quiet
+
+    def _print(self, s):
+        """Print iff the quite flag is not set to True"""
+        if self.quiet:
+            return
+        print(s)
 
     def update_wikitext(self):
+        """This will be run iff update_wikitext isn't overridden in a subclass"""
         self.prioritize_plaintext = True
 
     def update_plaintext(self):
+        """This will be run iff update_plaintext isn't overridden in a subclass"""
         self.prioritize_wikitext = True
 
     def run(self):
@@ -34,7 +55,7 @@ class PageModifierBase(object):
             if self.startat_page and page.name == self.startat_page:
                 self.passed_startat = True
             if not self.passed_startat:
-                print("Skipping page %s, before startat" % page.name)
+                self._print("Skipping page %s, before startat" % page.name)
                 continue
             lmt += 1
             self.current_text = page.text()
@@ -44,10 +65,10 @@ class PageModifierBase(object):
             self.update_wikitext()
             newtext = str(self.current_wikitext)
             if newtext != self.current_page.text() and not self.prioritize_plaintext:
-                print('Saving page %s...' % page.name)
+                self._print('Saving page %s...' % page.name)
                 page.save(newtext, summary=self.summary)
             elif self.current_text != self.current_page.text():
-                print('Saving page %s...' % page.name)
+                self._print('Saving page %s...' % page.name)
                 page.save(self.current_text, summary=self.summary)
             else:
-                print('Skipping page %s...' % page.name)
+                self._print('Skipping page %s...' % page.name)
