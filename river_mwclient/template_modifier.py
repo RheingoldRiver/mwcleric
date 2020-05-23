49ece9a22742031171366116706b9d150c47966d
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-05-23 15:35:11 -0500

    add support for quiet

--- river_mwclient/template_modifier.py
+++ river_mwclient/template_modifier.py
@@ -5,12 +5,14 @@ from .wiki_client import WikiClient
 
 class TemplateModifierBase(PageModifierBase):
     def __init__(self, site: WikiClient, template, page_list=None, title_list=None, limit=-1, summary=None,
+                 quiet=False,
                  startat_page=None):
         self.template_name = template
         self.current_template = None
         if not title_list:
             page_list = page_list if page_list else site.pages_using(template)
         super().__init__(site, page_list=page_list, title_list=title_list, limit=limit, summary=summary,
+                         quiet=quiet,
                          startat_page=startat_page)
     
     def update_wikitext(self):
