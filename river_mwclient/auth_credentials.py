20aa81b6ffa20c577f5cfb058284e4039682f1a8
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-06-27 10:07:56 -0500

    support non-local user files

--- river_mwclient/auth_credentials.py
+++ river_mwclient/auth_credentials.py
@@ -11,6 +11,10 @@ class AuthCredentials(object):
         """
         Stores username and password for future use with a WikiClient.
         Specify either user_file or both username and password.
+        
+        If using a file, files can be either located in the same directory as the code,
+        or in the config directory of the user. If files in both locations exist,
+        files in the former location will trump those in the latter.
 
         :param username: Username, this must include an @ if using a bot password
         :param password: Password, this is the actual value of the password, not the "name" of a "bot password"
@@ -25,6 +29,12 @@ class AuthCredentials(object):
             if path.exists(pwd_file) and path.exists(usr_file):
                 self.password = open(pwd_file).read().strip()
                 self.username = open(usr_file).read().strip()
+                return
+            pwd_path = path.join(path.expanduser('~'), '.config', 'river_mwclient', pwd_file)
+            usr_path = path.join(path.expanduser('~'), '.config', 'river_mwclient', usr_file)
+            if path.exists(pwd_path) and path.exists(usr_path):
+                self.password = open(pwd_path).read().strip()
+                self.username = open(usr_path).read().strip()
                 return
             pwd_key = 'WIKI_PASSWORD_{}'.format(user_file.upper())
             usr_key = 'WIKI_USERNAME_{}'.format(user_file.upper())
