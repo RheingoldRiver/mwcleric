e61cf3f9d326e452233e26431aff01f33ffe3dce
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-07-01 07:30:15 -0500

    add docstring

--- river_mwclient/wiki_time.py
+++ river_mwclient/wiki_time.py
@@ -3,6 +3,19 @@ import dateutil.parser
 
 
 class WikiTime(object):
+    """
+    Leaguepedia and the other esports wikis us an EXTREMELY simplified time zone model.
+    In this model, there are only three time zones: PST, CET, and KST.
+    Additionally, the wiki itself does not know anything about daylight savings;
+    instead, it relies on the user supplying DST metadata along with every timestamp.
+    DST metadata can be supplied with three options: Yes, Spring [or Fall], or No.
+    Yes means that both CET and PST have DST; No means that neither does, and
+    Spring/Fall mean that PST does but CET does not. (KST, sanely, never has DST.)
+    For simplicity, even in the fall, we return `Spring`.
+    
+    This class reduces timestamps to this simple model and provides information
+    in the format expected by the esports wikis.
+    """
     pst = timezone('America/Los_Angeles')
     cet = timezone('Europe/Berlin')
     kst = timezone('Asia/Seoul')
