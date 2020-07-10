11263c09301c865bce74a11d904d1151e4f2d5d7
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-07-10 09:54:53 -0500

    shit its recursive not flat

--- river_mwclient/template_modifier.py
+++ river_mwclient/template_modifier.py
@@ -6,11 +6,11 @@ from .wiki_client import WikiClient
 class TemplateModifierBase(PageModifierBase):
     def __init__(self, site: WikiClient, template, page_list=None, title_list=None, limit=-1, summary=None,
                  quiet=False, lag=0, tags=None, skip_pages=None,
-                 flat=False,
+                 recursive=False,
                  startat_page=None):
         self.template_name = template
         self.current_template = None
-        self.flat=flat
+        self.recursive=recursive
         if not title_list:
             page_list = page_list if page_list else site.pages_using(template)
         super().__init__(site, page_list=page_list, title_list=title_list, limit=limit, summary=summary,
@@ -18,7 +18,7 @@ class TemplateModifierBase(PageModifierBase):
                          startat_page=startat_page)
     
     def update_wikitext(self, wikitext):
-        for template in wikitext.filter_templates(flat=self.flat):
+        for template in wikitext.filter_templates(recursive=self.recursive):
             if template.name.matches(self.template_name):
                 self.current_template = template
                 self.update_template(template)
