c02fb59fce11de732c8464233379725a323df3e3
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-11-12 13:04:50 -0600

    fix _retry_login

--- river_mwclient/wiki_client.py
+++ river_mwclient/wiki_client.py
@@ -182,7 +182,8 @@ class WikiClient(object):
         try:
             page.edit(text, summary=summary, minor=minor, bot=bot, section=section, **kwargs)
         except self.write_errors:
-            self._retry_login(self._retry_save(), 'edit')
+            self._retry_login(self._retry_save, 'edit', page=page, summary=summary, minor=minor,
+                              bot=bot, section=section, **kwargs)
 
     @staticmethod
     def _retry_save(**kwargs):
