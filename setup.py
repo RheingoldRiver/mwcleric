db5d106245eaccaef5901a1acca0e256223cec20
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-03-18 07:19:20 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="river_mwclient",
-    version="0.2.0",
+    version="0.3.0",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="River's mwclient wrapper",
