1c61869b50106cf29ef39edd565869c4fac315f8
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2021-04-17 05:30:32 -0500

    Support interactive login & json files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mwcleric",
-    version="0.4.0",
+    version="0.4.1",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="River's mwclient wrapper",
